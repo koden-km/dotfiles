2f796cc75593dc3d9f5d55b65ea3c184f672d79e
Kevin Millar <koden.km@gmail.com>
2013-05-21 21:57:43 +1000

    Changed to the ports Go install.

--- lib/path.bash
+++ lib/path.bash
@@ -2,6 +2,6 @@ PATH="/opt/local/bin:/opt/local/sbin:/usr/local/pear/bin:$PATH"
 
 # Go lang
 # i think GOROOT of "/usr/local/go" is optional because that is the default
-export GOROOT=/usr/local/go
+export GOROOT=/opt/local/go
 # Colon seperated list of a workspaces.
 export GOPATH=$HOME/Documents/Go:$HOME/Development/Go
