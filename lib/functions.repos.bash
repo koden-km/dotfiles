2f233a5912c64b750aa68eabebd0d06bf96a8eab
Kevin Millar <koden.km@gmail.com>
2013-10-04 16:02:01 +1000

    Converted tabs to spaces

--- lib/functions.repos.bash
+++ lib/functions.repos.bash
@@ -14,7 +14,7 @@ function rclone {
     mkdir -p "$(dirname $dir)"
     hub clone -p $repo $dir
 
-	rcd-reindex
+    rcd-reindex
     rcd $repo
 }
 
@@ -52,20 +52,20 @@ function rcd {
 
     if [[ $count == "" ]]; then
         for base in $(git-directories); do
-			if [ -d $base ]; then
-				matches=$(find $base -mindepth 2 -maxdepth 2 -type d -iname $name)
-				count=$(echo $matches | wc -w | tr -d ' ')
+            if [ -d $base ]; then
+                matches=$(find $base -mindepth 2 -maxdepth 2 -type d -iname $name)
+                count=$(echo $matches | wc -w | tr -d ' ')
 
-				if [ $count -gt 0 ]; then
-					break
-				fi
-			fi
+                if [ $count -gt 0 ]; then
+                    break
+                fi
+            fi
         done
     fi
 
-	if [[ $count == "" ]]; then
-		count=0
-	fi
+    if [[ $count == "" ]]; then
+        count=0
+    fi
 
     if [ $count -eq 1 ]; then
         cd $matches
@@ -86,23 +86,23 @@ function rcd {
 }
 
 function rcd-reindex {
-	GIT_DIR_CACHE=""
-	for base in $(git-directories); do
-		if [ -d $base ]; then
-			for dir in $(find $base -mindepth 2 -maxdepth 2 -type d); do
-				repo=$(echo $dir } rev | cut -d/ -f1-2 | rev)
-				GIT_DIR_CACHE="$repo $(basename "$repo") $GIT_DIR_CACHE"
-			done
-		fi
-	done
+    GIT_DIR_CACHE=""
+    for base in $(git-directories); do
+        if [ -d $base ]; then
+            for dir in $(find $base -mindepth 2 -maxdepth 2 -type d); do
+                repo=$(echo $dir } rev | cut -d/ -f1-2 | rev)
+                GIT_DIR_CACHE="$repo $(basename "$repo") $GIT_DIR_CACHE"
+            done
+        fi
+    done
 }
 
 function __rcd-completion {
-	if [[ "$GIT_DIR_CACHE" == "" ]]; then
-		rcd-reindex
-	fi
+    if [[ "$GIT_DIR_CACHE" == "" ]]; then
+        rcd-reindex
+    fi
 
-	COMPREPLY=( $(compgen -W "$GIT_DIR_CACHE" -- ${COMP_WORDS[COMP_CWORD]}) )
+    COMPREPLY=( $(compgen -W "$GIT_DIR_CACHE" -- ${COMP_WORDS[COMP_CWORD]}) )
 }
 
 complete -F __rcd-completion rcd
