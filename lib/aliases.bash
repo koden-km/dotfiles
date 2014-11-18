26fd67805c60521807d85181b3fb845890fe0767
Kevin Millar <koden.km@gmail.com>
2014-11-18 10:00:13 +1000

    Added ctags language aliases

--- lib/aliases.bash
+++ lib/aliases.bash
@@ -3,6 +3,15 @@ alias ll="ls -al"
 alias grep="grep --color=auto"
 alias less="less -R"
 
+# ctags
+alias ctags-php='ctags -R --languages=PHP'
+alias ctags-js='ctags -R --languages=JavaScript'
+alias ctags-java='ctags -R --languages=Java'
+alias ctags-go='ctags -R --languages=Go'
+alias ctags-cpp='ctags -R --languages=C++'
+alias ctags-csharp='ctags -R --languages=C#'
+alias ctags-py='ctags -R --languages=Python'
+
 if [[ $(uname) == "Darwin" ]]; then
     alias mac-clean-dsstore="find . -name '.DS_Store' -delete"
     # fixing 'open with' duplicates may also need to: killall Finder
