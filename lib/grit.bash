021279abd91229f5989e81e62352ad3e0b0a3205
Kevin Millar <koden.km@gmail.com>
2017-03-29 09:34:50 +1000

    Grit shell completion update.

--- lib/grit.bash
+++ lib/grit.bash
@@ -1,40 +1,28 @@
-# Store the location of the real grit binary.
-GRIT_BIN=$(which grit)
+if [ -x grit ]; then
 
-if [ -f "$GRIT_BIN" ]; then
+	# Setup grit shell integration:
+	# $ eval "$(grit shell-integration)"
+	# Or manually put the output of 'grit shell-integration' below to avoid running eval.
 
-    # Make a bash function named grit so that we can execute our directory changes
-    # in the current shell.
-    grit() {
-        case $1 in
-            # Intercept commands that print directories to STDOUT.
-            clone|cd)
-                local dir=$($GRIT_BIN "$@")
-                if [ -d "$dir" ]; then
-                    cd "$dir"
-                    return 0
-                fi
+	# ---Begin Manual---
+	grit() {
+		local file="$(mktemp)"
+		trap "rm -f '$file'" EXIT
+		"/Users/kevin/bin/grit" --with-shell-integration="$file" "$@" && source "$file"
+		return $?
+	}
 
-                return 1
-            ;;
-            # Pass through other commands unchanged.
-            *)
-                $GRIT_BIN "$@"
-                return $?
-            ;;
-        esac
-    }
+	# Setup autocompletion using the real binary.
+	_grit_bash_autocomplete() {
+		local cur opts base
+		COMPREPLY=()
+		cur="${COMP_WORDS[COMP_CWORD]}"
+		opts=$(GRIT_COMP_WORDS="${COMP_WORDS[@]}" "/Users/kevin/bin/grit" ${COMP_WORDS[@]:1:$COMP_CWORD} --generate-bash-completion)
+		COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+		return 0
+	}
 
-    # Setup autocompletion using the real binary.
-    _grit_bash_autocomplete() {
-        local cur opts base
-        COMPREPLY=()
-        cur="${COMP_WORDS[COMP_CWORD]}"
-        opts=$($GRIT_BIN ${COMP_WORDS[@]:1:$COMP_CWORD} --generate-bash-completion)
-        COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
-        return 0
-    }
-
-    complete -F _grit_bash_autocomplete grit
+	complete -F _grit_bash_autocomplete grit
+	# ---End Manual---
 
 fi
