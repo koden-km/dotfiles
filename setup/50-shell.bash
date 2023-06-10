7a8b240ec30c156dfd6ae181bf67b3e5262a4ae1
Kevin Millar <kevin@codeworx.com.au>
2023-06-10 20:10:03 +1000

    Add fonts

--- setup/50-shell.bash
+++ setup/50-shell.bash
@@ -2,19 +2,26 @@ set -e
 
 echo "Configuring shell..."
 
-if [[ "/usr/local/bin/zsh" == "$SHELL" ]]; then
+# Note: brew bin location changes based on OS/platform
+# - Intel:         /usr/local/bin/zsh
+# - Apple Silicon: /opt/homebrew/bin/zsh
+
+if [[ "/opt/homebrew/bin/zsh" == "$SHELL" ]]; then
     echo "Brew zsh is already the current shell."
 else
-    if grep -Fxq /usr/local/bin/zsh /etc/shells; then
+    if grep -Fxq /opt/homebrew/bin/zsh /etc/shells; then
         echo "Brew zsh is already a standard shell."
     else
         echo "Adding brew zsh as a standard shell..."
-        sudo sh -c 'echo /usr/local/bin/zsh >> /etc/shells'
+        sudo sh -c 'echo /opt/homebrew/bin/zsh >> /etc/shells'
     fi
 
     echo "Setting brew zsh as the current shell..."
-    chsh -s /usr/local/bin/zsh
+    chsh -s /opt/homebrew/bin/zsh
 fi
+
+echo "Installing fonts ..."
+cp $HOME/dotfiles/files/*.* $HOME/Library/Fonts/
 
 if [[ -e "$HOME/.zprezto" ]]; then
     echo "Prezto already installed."
